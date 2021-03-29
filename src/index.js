098701ac2680248332e74f9cb0ff3acb3ac9c946
stupidwolfy <dsl526b@gmail.com>
2021-03-29 09:01:13 +0000

    c

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-# Use an official Node 12.7.0 runtime as a parent image
-FROM node:12.7.0-alpine
+const express = require('express')
+const app = express()
+const port = 3000
 
-# Set the working directory to /app
-WORKDIR '/app'
+app.get('/', (req, res) => res.send('Lab 9 Done !!'))
 
-# Copy package.json to the working directory
-COPY package.json .
-
-# Install any needed packages specified in package.json
-RUN yarn
-
-# Copying the rest of the code to the working directory
-COPY index.js .
-
-# Make port 3000 available to the world outside this container
-EXPOSE 3000
-
-# Run index.js when the container launches
-CMD ["node", "index.js"]
-
+app.listen(port, "0.0.0.0", () => console.log(`Web app listening on port ${port}!`))
